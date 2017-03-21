25d75d27efbeb47c8a44b2e1c2900f36e29cf257
warnov <warnov@UNDERBEAST>
2017-03-21 17:35:19 -0500

    Assembling Azure Storage Connection String tool completed

---
+++ AzTools/AzTools/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0) help();
+            else
+            switch(args[0].ToLower())
+            {
+                case "sc":
+                    SC(args);
+                    break;
+                case "help":
+                default:
+                    help();
+                    break;
+            }
+            Console.ReadKey();
+        }
+
+        private static void help()
+        {
+            Console.WriteLine("Available commands:\nsc: Assemble Azure Storage Connection String. Use: azTools sc [accountName] [pak]");            
+        }
+
+        private static void SC(string[] args)
+        {
+            var accountName = args[1];
+            var PAK = args[2];
+            Console.WriteLine($"Assembled connection string:\n\nDefaultEndpointsProtocol=https;AccountName={accountName};AccountKey={PAK}");
+        }
+    }
+}
